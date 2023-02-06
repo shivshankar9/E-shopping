4c1833ca818105b925502956393aeadd41af00a5
shivshankar9 <shivshankarkumar281@gmail.com>
2023-02-07 00:44:15 +0530

    ,,

--- Itdev/register.php
+++ Itdev/register.php
@@ -39,3 +39,59 @@
 </main>
 </body>
 </html>
+<?php
+
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get the form data
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password2 = $_POST['password2'];
+    $agree = $_POST['agree'];
+
+    // Validate the form data here...
+
+    // If the form data is valid, then send the email
+    if (/* form data is valid */) {
+        // Use PHPMailer to send the email
+        use PHPMailer\PHPMailer\PHPMailer;
+        use PHPMailer\PHPMailer\Exception;
+
+        require 'path/to/vendor/autoload.php';
+
+        $mail = new PHPMailer(true);
+
+        try {
+            // Server settings
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'smtp.example.com';                    // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'your-email@example.com';          // SMTP username
+            $mail->Password = 'your-email-password';              // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 587;                                    // TCP port to connect to
+
+            // Recipients
+            $mail->setFrom('from@example.com', 'From Name');
+            $mail->addAddress('to@example.com', 'To Name');     // Add a recipient
+
+            // Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'New User Registration';
+            $mail->Body = 'A new user has registered with the following details:<br><br>' .
+                'Username: ' . $username . '<br>' .
+                'Email: ' . $email . '<br>' .
+                'Password: ' . $password . '<br>' .
+                'Agree: ' . $agree;
+
+            $mail->send();
+            echo 'An email has been sent to notify you of the new user registration.';
+        } catch (Exception $e) {
+            echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+        }
+    }
+}
+
+?>
